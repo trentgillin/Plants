16d4d2bef9580ac183a6e753f1038e826aae15e1
trentgillin <trentgillin@gmail.com>
2019-09-28 11:01:30 -0400

    added code to rename images

--- converting_images.R
+++ converting_images.R
@@ -16,3 +16,13 @@ convert_jpg <- function(img) {
 images <- list.files("Invasive_Plants/images", pattern = ".jpg|.JPG")
 
 pngs <- map(images, convert_jpg)
+
+# want to rename them to get rid of numbers in front
+images <- list.files("Invasive_Plants/images", pattern = "*.png")
+paths <- paste0("Invasive_Plants/images/", images)
+
+rename_images <- function(image_name) {
+  file.rename(image_name, str_remove(image_name, "[:digit:]+px-"))
+}
+
+result <- map(paths, rename_images)
